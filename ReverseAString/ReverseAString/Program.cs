e5eff8bc68e535bfd7131ebf04d90ac5ca74af22
PrinceVladCoder <38655139+PrinceVladCoder@users.noreply.github.com>
2018-05-08 17:25:51 +0200

    Add files via upload

---
+++ ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Please enter your name:");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hallo " + userName + " lets reverse your name");
+            Console.Clear();
+            Console.WriteLine(userName);
+            char[] inputArray = userName.ToCharArray();
+            Array.Reverse(inputArray);
+            string reversed = new string (inputArray);
+            Console.WriteLine(reversed);
+        }  
+    }
+}
